0e42db12c33f794355da7f349c271fa3c70b3436
Mikhail <mikhailmos22@yandex.ru>
2021-10-06 20:06:24 +0300

    forgotten initial commit

--- WeatherApp_test/ViewController.swift
+++ WeatherApp_test/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func nextVC(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "CitiesViewController", bundle: nil)
+        guard let citiesVC = storyboard.instantiateViewController(withIdentifier: "CitiesViewController") as? CitiesViewController else { return }
 
+        self.navigationController?.pushViewController(citiesVC, animated: true)
+        
+    }
+    
 }
 
